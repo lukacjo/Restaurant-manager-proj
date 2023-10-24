7dd61cb01d74ff7d674a64fa29379176159c36bf
lukacjo <85407127+lukacjo@users.noreply.github.com>
2023-10-24 14:08:40 +0200

    creating conditions and flash msg for singup form

--- website/auth.py
+++ website/auth.py
@@ -1,10 +1,33 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash, redirect
+
 
 auth = Blueprint("auth", __name__)
 
 @auth.route("/signup", methods=['POST', 'GET'])
 def signup():
+    if request.method == "POST":
+        email = request.form.get('email')
+        firstname = request.form.get('firstname')
+        lastname = request.form.get('lastname')
+        password = request.form.get('password')
+        password2 = request.form.get('password2')
+        
+        
+        if len(email) < 4:
+            flash('Email must be greater that 3 charachters', category='error')
+        elif len(firstname) < 2:
+            flash('First name must be greater that 1 charachters', category='error')
+        elif len(lastname) < 2:
+            flash('Last name must be greater that 1 charachters', category='error')
+        elif password != password2:
+            flash('password must match', category='error')
+        elif len(password) < 3:
+            flash('Password must be greater that 2 charachters', category='error')
+        else:
+            flash('Account created!', category='success')
+        
     return render_template("signup.html")
+
 
 @auth.route("/login", methods=['POST', 'GET'])
 def login():
