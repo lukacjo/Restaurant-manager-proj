688c1d9d1eba685cac209ce630d8e20fe84a8b6c
lukacjo <85407127+lukacjo@users.noreply.github.com>
2023-10-24 12:59:37 +0200

    sign up page

--- website/views.py
+++ website/views.py
@@ -2,6 +2,6 @@ from flask import Blueprint, render_template
 
 views = Blueprint("views", __name__)
 
-@views.route("/")
-def home():
-    return render_template("navbar.html")
+@views.route("/signup")
+def signup():
+    return render_template("signup.html")
