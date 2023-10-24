bf92473fe986cc83d8efd8f592fe9afe5f32f2b4
lukacjo <85407127+lukacjo@users.noreply.github.com>
2023-10-24 13:05:29 +0200

    moving signup and login to auth.py

--- website/auth.py
+++ website/auth.py
@@ -1,3 +1,11 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 auth = Blueprint("auth", __name__)
+
+@auth.route("/signup", methods=['POST', 'GET'])
+def signup():
+    return render_template("signup.html")
+
+@auth.route("/login", methods=['POST', 'GET'])
+def login():
+    return render_template("login.html")
--- website/views.py
+++ website/views.py
@@ -2,10 +2,3 @@ from flask import Blueprint, render_template
 
 views = Blueprint("views", __name__)
 
-@views.route("/signup")
-def signup():
-    return render_template("signup.html")
-
-@views.route("/login")
-def login():
-    return render_template("login.html")
