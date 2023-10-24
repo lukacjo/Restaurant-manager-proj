97d109a6c29322878c4d36f1ee15006c22cb8beb
lukacjo <85407127+lukacjo@users.noreply.github.com>
2023-10-24 15:47:16 +0200

    adding user data to database&hasing password

--- website/auth.py
+++ website/auth.py
@@ -1,4 +1,8 @@
-from flask import Blueprint, render_template, request, flash, redirect
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from .models import User, Note
+from werkzeug.security import generate_password_hash, check_password_hash
+from . import db
+
 
 
 auth = Blueprint("auth", __name__)
@@ -12,8 +16,10 @@ def signup():
         password = request.form.get('password')
         password2 = request.form.get('password2')
         
-        
-        if len(email) < 4:
+        user = User.query.filter_by(email=email).first()
+        if user:
+            flash('This user already exists', category='error')
+        elif len(email) < 4:
             flash('Email must be greater that 3 charachters', category='error')
         elif len(firstname) < 2:
             flash('First name must be greater that 1 charachters', category='error')
@@ -24,8 +30,13 @@ def signup():
         elif len(password) < 3:
             flash('Password must be greater that 2 charachters', category='error')
         else:
-            flash('Account created!', category='success')
-        
+            new_user = User(email=email, firstname=firstname, lastname=lastname, password=generate_password_hash(password, method="sha256"))
+            db.session.add(new_user)
+            db.session.commit()
+            
+            flash('Account created', category="success")
+            return redirect(url_for('views.home'))
+               
     return render_template("signup.html")
 
 
--- website/views.py
+++ website/views.py
@@ -2,3 +2,6 @@ from flask import Blueprint, render_template
 
 views = Blueprint("views", __name__)
 
+@views.route("/home")
+def home():
+    return render_template("notes.html")
