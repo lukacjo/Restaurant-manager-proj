d18465571afd08e8ef935e49f82ac85efaa46f5d
lukacjo <85407127+lukacjo@users.noreply.github.com>
2023-10-24 13:01:47 +0200

    log in page

--- website/views.py
+++ website/views.py
@@ -5,3 +5,7 @@ views = Blueprint("views", __name__)
 @views.route("/signup")
 def signup():
     return render_template("signup.html")
+
+@views.route("/login")
+def login():
+    return render_template("login.html")
