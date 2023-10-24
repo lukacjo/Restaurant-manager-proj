03b286621ed669bfa2802d59bb8ead88838b6321
lukacjo <85407127+lukacjo@users.noreply.github.com>
2023-10-24 12:35:32 +0200

    registering blueprints & getting rid of src file

--- website/__init__.py
+++ website/__init__.py
@@ -3,4 +3,11 @@ from flask import Flask
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'eh2WGSwY@HwO$!S!j32EM*9$36zKJCyvrvu#fRVV3rgs$3@H&whPGXwknMyW#qypW%E#D8yhTPDq$m##Ho6wUkNdIeuWozI8dZM' # secret key jest tu widoczny bo jest to tylko do portfolio inaczej byłby ukryty ze względów bezpieczeństwa, moze jeszcze tym sie zajmę ale zobaczę 
+    
+    from .views import views
+    from .auth import auth
+    
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
+
     return app
