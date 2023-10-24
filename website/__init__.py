d4b3b7b122d5adcedc5a4f4c4b4480d71cec5358
lukacjo <85407127+lukacjo@users.noreply.github.com>
2023-10-24 14:15:31 +0200

    creating database with sqlalchemy and sqlite

--- website/__init__.py
+++ website/__init__.py
@@ -1,9 +1,17 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+
+db = SQLAlchemy()
+DB_NAME = "database.db" # ustawienei nazwy bazy danych
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'eh2WGSwY@HwO$!S!j32EM*9$36zKJCyvrvu#fRVV3rgs$3@H&whPGXwknMyW#qypW%E#D8yhTPDq$m##Ho6wUkNdIeuWozI8dZM' # secret key jest tu widoczny bo jest to tylko do portfolio inaczej byłby ukryty ze względów bezpieczeństwa, moze jeszcze tym sie zajmę ale zobaczę 
     
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}' # pokazuje flaskowi ze uzywam bazy danych i gdzie ona jest
+    db.init_app(app)
+   
     from .views import views
     from .auth import auth
     
